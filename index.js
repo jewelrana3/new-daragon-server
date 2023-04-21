a7b43239fd2623726b92c5aa2500c988e44d5d19
jewelrana3 <juyelrana7752@gmail.com>
2023-04-21 11:15:20 +0600

    spesific data

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const port = process.env.PORT || 3000;
 
 const catacory = require('./data/catacory.json')
+const news = require('./data/news.json')
 app.use(cors())
 
 app.get('/',(req,res)=>{
@@ -12,6 +13,29 @@ app.get('/',(req,res)=>{
 
 app.get('/catacory',(req,res)=>{
     res.send(catacory)
+})
+
+app.get('/news',(req,res)=>{
+    res.send(news)
+})
+
+app.get('/news/:id',(req,res)=>{
+    const id = req.params.id;
+    console.log(id)
+    const selected = news.find(n => n._id === id)
+    res.send(selected)
+})
+
+app.get('/catacory/:id',(req,res)=>{
+    const id = parseInt(req.params.id);
+    console.log(id)
+    if(id === 0){
+        res.send(news)
+    }
+    else {
+        const catacoryNews = news.filter(n => parseInt(n.category_id) === id);
+        res.send(catacoryNews)
+    }
 })
 app.listen(port,()=>{
     console.log(`daragon is a projceet ${port}`)
